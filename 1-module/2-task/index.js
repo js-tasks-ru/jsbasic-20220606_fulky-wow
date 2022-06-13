062098e6aa70064ffa8e0e1bf5e5593d1b450559
Dmitry Tarasov <fizzardinio@gmail.com>
2022-06-13 14:57:17 +0300

    2-2.3

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name.length >= 4 && !name.includes(' ') && name != null) {
+  if (name.length >= 4 && !name.includes(' ') && !(name === null)) {
     return true
   } else  {
     return false
