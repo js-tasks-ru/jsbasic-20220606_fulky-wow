e8a7e6134dd5f1e7523ddde72c8d785d3cf73fd5
Dmitry Tarasov <fizzardinio@gmail.com>
2022-06-20 22:14:59 +0300

    3-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  const res = str.split(' ')
+    .map(string => +string);
+  const num = [];
+  for (let key of res) {
+    if (!Number.isNaN(key)) {
+      num.push(key)
+    }
+  }
+  const minNum = Math.min(...num);
+  const maxNum = Math.max(...num);
+  return {
+    min: minNum,
+    max: maxNum,
+  };
 }
